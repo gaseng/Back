19a85c7e42a73c9a798148032984de72588e6e18
yeowon0411 <dudnjsdl11@naver.com>
2023-10-30 14:18:47 +0900

    Feat: 내가 쓴 글 확인

--- src/main/java/com/gaseng/sharehouse/repository/SharehouseRepository.java
+++ src/main/java/com/gaseng/sharehouse/repository/SharehouseRepository.java
@@ -2,12 +2,18 @@ package com.gaseng.sharehouse.repository;
 
 import com.gaseng.sharehouse.domain.Sharehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SharehouseRepository extends JpaRepository<Sharehouse,Long> {
     Optional<Sharehouse> findByShrId(Long shrId);
+
+    @Query(value = "SELECT * FROM sharehouse WHERE mem_id = :memId ORDER BY created_date DESC LIMIT :size OFFSET :page * :size", nativeQuery = true)
+    List<Sharehouse> getSharehousesByCursorScroll(Long memId, @Param("page") int page, @Param("size") int size);
 
 }

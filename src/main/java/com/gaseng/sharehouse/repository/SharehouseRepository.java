3d032f17e9ec940629ad300dda454b624cb26681
yeowon0411 <dudnjsdl11@naver.com>
2023-11-05 20:04:28 +0900

    Feat: 쉐어하우스 글 전체 조회 & 상세 조회, 스크랩 게시물 조회 API 구현

--- src/main/java/com/gaseng/sharehouse/repository/SharehouseRepository.java
+++ src/main/java/com/gaseng/sharehouse/repository/SharehouseRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,6 +17,8 @@ public interface SharehouseRepository extends JpaRepository<Sharehouse,Long> {
     Optional<Sharehouse> findByShrId(Long shrId);
     List<Sharehouse> findByMember(Member member);
     List<Sharehouse> findByMemberOrderByCreatedDateDesc(Member member);
+    @Query("SELECT s FROM Sharehouse s ORDER BY s.createdDate DESC")
+    List<Sharehouse> findAllOrderByCreatedDateDesc();
 
     Slice<SharehouseListResponse> findByMemberOrderByShrIdDesc(Member member, Pageable pageable);
 
